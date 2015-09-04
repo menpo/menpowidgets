fe6e4c7fdb3e359f04a6b1591d44aebdc4537937
Epameinondas Antonakos <antonakosn@gmail.com>
2015-09-04 15:55:09 +0100

    adds setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import versioneer
+
+
+setup(name='menpowidgets',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description="Menpo's Jupyter widgets for fancy visualization",
+      author='The Menpo Development Team',
+      author_email='james.booth08@imperial.ac.uk',
+      packages=find_packages(),
+      install_requires=['menpo>=0.5.1,<0.6',
+                        'menpofit>=0.2.1,<0.3'],
+      package_data={'menpo': 'logos/*'}
+      )
